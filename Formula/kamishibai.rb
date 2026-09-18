fd367aa2a133c9842c181de13162d941cdad0a9f
Anatoly Chichikov <treks.mbox@gmail.com>
2026-09-18 22:49:17 +0300

    kamishibai 1.13.2
    Closes #32.
    
    Signed-off-by: Anatoly Chichikov <2089536+anatoly-chichikov@users.noreply.github.com>

--- Formula/kamishibai.rb
+++ Formula/kamishibai.rb
@@ -2,16 +2,9 @@ class Kamishibai < Formula
   desc "Turn a list of words into an illustrated anki deck with native-speaker audio"
   homepage "https://github.com/anatoly-chichikov/kamishibai"
 
-  url "https://github.com/anatoly-chichikov/kamishibai/archive/refs/tags/v1.13.1.tar.gz"
-  sha256 "c7d989e63a780e4478a6380647b413bd3c2ac57665ce1e9ae6914b332bc2a7a8"
+  url "https://github.com/anatoly-chichikov/kamishibai/archive/refs/tags/v1.13.2.tar.gz"
+  sha256 "106e24eef879c7931c95e04014908ee086d114eff97e9eb5bc0ec2add6fc2445"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/anatoly-chichikov/homebrew-tap/releases/download/kamishibai-1.13.1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b7792665582e3c117d80f18e513d9836286cbea81838c56d743cd928c1a69015"
-    sha256 cellar: :any,                 arm64_linux:   "1ae13fc2c8b707592eb30a9785ca3a82ec0ec95aebc7e7f8e20f06cf63729658"
-    sha256 cellar: :any,                 x86_64_linux:  "8c3df8f8474abe3638b785b64ffde3576fe5cada950c4b050dfdcad893e846ab"
-  end
 
   depends_on "cmake" => :build
   depends_on "llvm" => :build
@@ -42,6 +35,6 @@ class Kamishibai < Formula
   end
 
   test do
-    assert_equal "kamishibai 1.13.1", shell_output("#{bin}/kamishibai --version").strip
+    assert_equal "kamishibai 1.13.2", shell_output("#{bin}/kamishibai --version").strip
   end
 end
