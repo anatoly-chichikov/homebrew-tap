2da2be642579edc080ce2ac787aeef652034e127
Anatoly Chichikov <2089536+anatoly-chichikov@users.noreply.github.com>
2026-09-18 20:05:50 +0000

    kamishibai: add 1.13.2 bottle.

--- Formula/kamishibai.rb
+++ Formula/kamishibai.rb
@@ -6,6 +6,13 @@ class Kamishibai < Formula
   sha256 "106e24eef879c7931c95e04014908ee086d114eff97e9eb5bc0ec2add6fc2445"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/anatoly-chichikov/homebrew-tap/releases/download/kamishibai-1.13.2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cc281f4e796031266ac532b365abb9dc68b1db90854cb369bba34c5705a749b4"
+    sha256 cellar: :any,                 arm64_linux:   "dadb5f4c080d3878e98e3ab3cc6a6548e0a4a4c317fe361c7a2964448a6735c8"
+    sha256 cellar: :any,                 x86_64_linux:  "bfb31c1dd9312b9dba386bd458c853379654b887547479b050da548472ad94e1"
+  end
+
   depends_on "cmake" => :build
   depends_on "llvm" => :build
   depends_on "pkgconf" => :build
